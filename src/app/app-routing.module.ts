ced1fcec8b40ee26d76fcd2e91bde4e0e0d732d4
onotaizee@gmail.com <onotaizee@gmail.com>
2023-09-30 15:09:28 +0100

    fix: azure page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'signup',
     pathMatch: 'full'
   },
   {
@@ -22,6 +22,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/auth/signup/signup.module').then( m => m.SignupPageModule)
   }
 ];
 
