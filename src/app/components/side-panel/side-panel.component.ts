fc885c06c77ee7afa8617ceee69471984344ceb6
darksuei <chrisraphael576@gmail.com>
2023-10-08 00:39:58 +0100

    (chore): Search Page

--- src/app/components/side-panel/side-panel.component.ts
+++ src/app/components/side-panel/side-panel.component.ts
@@ -5,26 +5,32 @@ import { Component, HostBinding, Input, OnInit } from '@angular/core';
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.scss'],
 })
-export class SidePanelComponent  implements OnInit {
-  settingsIconSrc = "../../../assets/icon/settings-outline.svg";
-  helpIconSrc = "../../../assets/icon/help-circle-outline.svg";
-  layerIconSrc = "../../../assets/icon/layer-custom.svg";
-  homeIconSrc = "../../../assets/icon/home-custom.svg";
-  pieIconSrc = "../../../assets/icon/pie-custom.svg";
-  bookIconSrc = "../../../assets/icon/book-custom.svg";
+export class SidePanelComponent implements OnInit {
+  settingsIconSrc = '../../../assets/icon/settings-outline.svg';
+  helpIconSrc = '../../../assets/icon/help-circle-outline.svg';
+  layerIconSrc = '../../../assets/icon/layer-custom.svg';
+  homeIconSrc = '../../../assets/icon/home-custom.svg';
+  pieIconSrc = '../../../assets/icon/pie-custom.svg';
+  bookIconSrc = '../../../assets/icon/book-custom.svg';
 
-  @Input() theme: string = "";
+  @Input() theme: string = '';
   @HostBinding('class.dark')
-  isDark:boolean = true;
+  isDark: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    if (this.theme == "dark") {
-      this.isDark = false
-      console.log("Dark")
-      document.getElementById("side-panel")?.classList.add("dark")
+    if (this.theme == 'dark') {
+      this.isDark = false;
+      console.log('Dark');
+      document.getElementById('side-panel')?.classList.add('dark');
     }
   }
+  checkButtonColor(theme: string) {
+    let buttonColor = theme !== 'dark' ? 'palette-dark' : 'dark';
 
+    return buttonColor;
+  }
+
+  onClick() {}
 }
