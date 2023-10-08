a3358345dddd9171f27b2093f6b0daffaf59e1e8
onotaizee@gmail.com <onotaizee@gmail.com>
2023-10-08 20:49:40 +0100

    fix: sidemenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,11 +20,11 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
   },
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
-  //   canActivate: [AuthGuard]
-  // },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    // canActivate: [AuthGuard]
+  },
   {
     path: 'search',
     loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule),
