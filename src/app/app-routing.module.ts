446ddc7e518cb7a2d370c2cc5311c41a3baa6a19
darksuei <chrisraphael576@gmail.com>
2023-10-13 19:16:44 +0100

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'signup',
+    redirectTo: 'welcome',
     pathMatch: 'full',
   },
   {
--- src/app/pages/analytics/analytics.page.ts
+++ src/app/pages/analytics/analytics.page.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnalyticsPage implements OnInit {
   theme = 'dark';
+  months = [
+    'January',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  days = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
   constructor() {}
 
   ngOnInit() {}
