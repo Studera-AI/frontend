ff97122e363012dcaba8db5017ee0dde6e991142
onotaizee@gmail.com <onotaizee@gmail.com>
2023-10-13 06:25:20 +0100

    fix: dynamic data selection based on JSON fields feat: swiperjs keyboard navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomePageModule),
-    canActivate: [AuthGuard],
+      canActivate: [AuthGuard],
   },
   {
     path: 'search',
