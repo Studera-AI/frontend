f44463c39391ba80f67c17acad137db3cf70f8db
darksuei <chrisraphael576@gmail.com>
2023-10-19 01:47:10 +0100

    Chore: User feedback form

--- src/app/interfaces/client.ts
+++ src/app/interfaces/client.ts
@@ -1,18 +1,22 @@
-export interface Client {
-}
+export interface Client {}
 
 export interface PromptRequest {
-  title: string
-  timeframe: string
-  type: string
+  title: string;
+  timeframe: string;
+  type: string;
 }
 
-export interface PromptData extends PromptRequest{
-  data: string
+export interface PromptData extends PromptRequest {
+  data: string;
 }
 
 export interface Test {
   question: string;
   options: string[];
-  answer: string
+  answer: string;
+}
+
+export interface FeedbackData {
+  email: string;
+  feedback: string;
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -6,100 +6,119 @@ import { Router } from '@angular/router';
 import { UtilityService } from './utility.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientService {
-
+  baseUrl = environment.baseUrl;
   promptData = signal<PromptData>({
-    title: "",
-    timeframe: "",
-    type: "",
-    data: ""
+    title: '',
+    timeframe: '',
+    type: '',
+    data: '',
   });
 
-  testData = signal<Test[]>([])
+  testData = signal<Test[]>([]);
 
   userLearnings = signal<PromptData[]>([]);
 
-  constructor(private http: HttpClient, private router: Router, private utilSrv: UtilityService) { }
-
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private utilSrv: UtilityService,
+    private httpClient: HttpClient
+  ) {}
 
   sendPromptRequest(data: PromptRequest) {
-    console.log("SENDING");
+    console.log('SENDING');
     this.utilSrv.promptLoading.set(true);
     this.http.post(`${environment.baseUrl}/resource`, data).subscribe({
       next: (r: any) => {
-        console.log(r)
-        let promptData: PromptData = {...data, data: r.data }
+        console.log(r);
+        let promptData: PromptData = { ...data, data: r.data };
         console.log(promptData);
         this.promptData.set(promptData);
         this.refreshLearnings();
-        this.router.navigate(["/home"]);
-
+        this.router.navigate(['/home']);
       },
       error: (e) => {
         this.utilSrv.promptLoading.set(false);
-        console.log(e)
+        console.log(e);
       },
       complete: () => {
-        console.log("COMPLETED!")
+        console.log('COMPLETED!');
         this.utilSrv.promptLoading.set(false);
-
-      }
-    })
+      },
+    });
   }
 
   private refreshLearnings() {
-    localStorage.removeItem("learnings");
+    localStorage.removeItem('learnings');
   }
 
   getUserLearnings() {
-    let learnings = localStorage.getItem("learnings");
+    let learnings = localStorage.getItem('learnings');
 
     if (learnings) {
       this.userLearnings.set(JSON.parse(learnings));
       return;
     }
     this.http.get(`${environment.baseUrl}/user`).subscribe({
-      next:(r: any) => {
+      next: (r: any) => {
         console.log(r);
-        localStorage.setItem("learnings", JSON.stringify(r.user.learnings))
+        localStorage.setItem('learnings', JSON.stringify(r.user.learnings));
         this.userLearnings.set(r.user.learnings);
       },
       error: (e) => {
-        console.log(e)
+        console.log(e);
       },
       complete: () => {
-        console.log("COMPLETED!")
-      }
-    })
+        console.log('COMPLETED!');
+      },
+    });
   }
 
   generatePreviousLearning(data: PromptData) {
     this.promptData.set(data);
-    this.router.navigate(["/home"])
+    this.router.navigate(['/home']);
   }
 
   generateTests(day: number, title: string) {
     this.utilSrv.testsLoading.set(true);
-    this.http.post(`${environment.baseUrl}/test`,{
-      day, title
-    }).subscribe({
-      next:(r: any) => {
-        console.log(r);
-        this.testData.set(r.res.test || r.res.questions);
-        this.router.navigate(["/test/assessment"])
-        // localStorage.setItem("learnings", JSON.stringify(r.user.learnings))
-        // this.userLearnings.set(r.user.learnings);
+    this.http
+      .post(`${environment.baseUrl}/test`, {
+        day,
+        title,
+      })
+      .subscribe({
+        next: (r: any) => {
+          console.log(r);
+          this.testData.set(r.res.test || r.res.questions);
+          this.router.navigate(['/test/assessment']);
+          // localStorage.setItem("learnings", JSON.stringify(r.user.learnings))
+          // this.userLearnings.set(r.user.learnings);
+        },
+        error: (e) => {
+          console.log(e);
+          this.utilSrv.testsLoading.set(false);
+        },
+        complete: () => {
+          console.log('COMPLETED!');
+          this.utilSrv.testsLoading.set(false);
+        },
+      });
+  }
+
+  submitFeedback(data: any) {
+    this.httpClient.post(`${this.baseUrl}/feedback`, data).subscribe({
+      next: (result: any) => {
+        console.log('Submitted feedback');
+        console.log(result);
+        alert('Feedback submitted');
       },
       error: (e) => {
-        console.log(e)
-        this.utilSrv.testsLoading.set(false);
+        console.log(e);
       },
-      complete: () => {
-        console.log("COMPLETED!")
-        this.utilSrv.testsLoading.set(false);
-      }
-    })
+      complete: () => {},
+    });
   }
 }
